694b1534f04bfe1ac9580593a19e80e2f645a9c2
Colgate13 <gabreilbarros13@gmail.com>
2023-06-24 11:16:23 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Player player1 = new Player("Colgate", 100, 0);
+        Console.WriteLine(player1.Life);
+    }
+}
+
+abstract public class Human {
+  private int _life;
+  private string _name;
+
+  public Human(string name, int life) {
+    _name = name;
+    _life = life;
+  }
+  public int Life {
+    get { return _life; }
+    set { _life = value; }
+  }
+}
+
+public class Player : Human {
+  private int _score;
+
+  public Player(string name, int life, int score) : base(name, life) {
+    _score = score;
+  }
+  public int Score {
+    get { return _score; }
+    set { _score = value; }
+  }
+} 
